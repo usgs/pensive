352a09f78f8f5e556ebf23d56c900be134e2786d
Tom Parker <tparker@usgs.gov>
2015-08-16 20:53:16 -0800

    Unit tests are good?

---
+++ src/test/java/gov/usgs/volcanoes/pensive/PensiveTest.java
@@ -0,0 +1,14 @@
+package gov.usgs.volcanoes.pensive;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PensiveTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

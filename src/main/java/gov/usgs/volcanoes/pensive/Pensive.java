7311282d776816059455958ecc9854370d167017
Tom Parker <tparker@usgs.gov>
2015-08-18 08:24:15 -0800

    Rename page class

--- src/main/java/gov/usgs/volcanoes/pensive/Pensive.java
+++ src/main/java/gov/usgs/volcanoes/pensive/Pensive.java
@@ -35,225 +35,230 @@ import gov.usgs.volcanoes.pensive.schedule.RealtimePlotScheduler;
  */
 public class Pensive {
 
-    public static final boolean DEFAULT_WRITE_HTML = true;
-    public static final String DEFAULT_CONFIG_FILENAME = "pensive.config";
+	public static final boolean DEFAULT_WRITE_HTML = true;
+	public static final String DEFAULT_CONFIG_FILENAME = "pensive.config";
 
-    /** my logger */
-    private static final Logger LOGGER = LoggerFactory.getLogger(Pensive.class);
+	/** my logger */
+	private static final Logger LOGGER = LoggerFactory.getLogger(Pensive.class);
 
-    /** my configuration file */
-    private ConfigFile configFile;
+	/** my configuration file */
+	private ConfigFile configFile;
 
-    /** My SPA */
-    private Page page;
+	/** My SPA */
+	private PensiveWebApp webApp;
 
-    /** One plot scheduler per wave server */
-    private Map<String, AbstractPlotScheduler> plotScheduler;
+	/** One plot scheduler per wave server */
+	private Map<String, AbstractPlotScheduler> plotScheduler;
 
-    /**
-     * Class constructor
-     * 
-     * @param configFile
-     *            my config file
-     */
-    public Pensive(ConfigFile configFile) {
+	/**
+	 * Class constructor
+	 * 
+	 * @param configFile
+	 *            my config file
+	 */
+	public Pensive(ConfigFile configFile) {
 
-        this.configFile = configFile;
-        long now = System.currentTimeMillis();
-        configFile.put("applicationLaunch", "" + now);
-        LOGGER.info("Launching Pensive ({})", PensiveVersion.VERSION_STRING);
+		this.configFile = configFile;
+		long now = System.currentTimeMillis();
+		configFile.put("applicationLaunch", "" + now);
+		LOGGER.info("Launching Pensive ({})", PensiveVersion.VERSION_STRING);
 
-        page = new Page(configFile);
+		webApp = new PensiveWebApp(configFile);
 
-        boolean writeHtml = configFile.getBoolean("writeHtml", DEFAULT_WRITE_HTML);
-        if (writeHtml)
-            page.writeHTML();
-    }
+		boolean writeHtml = configFile.getBoolean("writeHtml", DEFAULT_WRITE_HTML);
+		if (writeHtml)
+			webApp.writeHTML();
+	}
 
-    public Pensive(ConfigFile configFile, Date startTime, Date endTime) {
-        super();
-    }
+	public Pensive(ConfigFile configFile, Date startTime, Date endTime) {
+		super();
+	}
 
-    /**
-     * Create one PlotScheduler per wave server, each running in its own thread.
-     */
-    private void createPlotSchedulers() {
-        plotScheduler = new HashMap<String, AbstractPlotScheduler>();
+	/**
+	 * Create one PlotScheduler per wave server, each running in its own thread.
+	 */
+	private void createPlotSchedulers() {
+		plotScheduler = new HashMap<String, AbstractPlotScheduler>();
 
-        for (String server : configFile.getList("waveSource")) {
-            ConfigFile c = configFile.getSubConfig(server, true);
-            LOGGER.info("Creating plot scheduler for " + server);
-            plotScheduler.put(server, new RealtimePlotScheduler(server, c));
-        }
-        assignSubnets();
-        pruneSchedulers();
-    }
+		for (String server : configFile.getList("waveSource")) {
+			ConfigFile c = configFile.getSubConfig(server, true);
+			LOGGER.info("Creating plot scheduler for " + server);
+			plotScheduler.put(server, new RealtimePlotScheduler(server, c));
+		}
+		assignSubnets();
+		pruneSchedulers();
+		schedulePlots();
+	}
 
-    /**
-     * Create one PlotScheduler per wave server, each running in its own thread.
-     */
-    private void createPlotSchedulers(Date startTime, Date endTime) {
-        plotScheduler = new HashMap<String, AbstractPlotScheduler>();
+	/**
+	 * Create one PlotScheduler per wave server, each running in its own thread.
+	 */
+	private void createPlotSchedulers(Date startTime, Date endTime) {
+		plotScheduler = new HashMap<String, AbstractPlotScheduler>();
 
-        for (String server : configFile.getList("waveSource")) {
-            ConfigFile c = configFile.getSubConfig(server, true);
-            LOGGER.info("Creating plot scheduler for " + server);
-            BackfillPlotScheduler ps = new BackfillPlotScheduler(server, c);
-            ps.setRange(startTime, endTime);
-            plotScheduler.put(server, ps);
-        }
-        assignSubnets();
-        pruneSchedulers();
-    }
+		for (String server : configFile.getList("waveSource")) {
+			ConfigFile c = configFile.getSubConfig(server, true);
+			LOGGER.info("Creating plot scheduler for " + server);
+			BackfillPlotScheduler ps = new BackfillPlotScheduler(server, c);
+			ps.setRange(startTime, endTime);
+			plotScheduler.put(server, ps);
+		}
+		assignSubnets();
+		pruneSchedulers();
+		schedulePlots();
+	}
 
-    /**
-     * Assign subnets to it wave server
-     */
-    private void assignSubnets() {
-        List<String> networks = configFile.getList("network");
-        if (networks == null)
-            throw new RuntimeException("No network directives found.");
+	/**
+	 * Assign subnets to it wave server
+	 */
+	private void assignSubnets() {
+		List<String> networks = configFile.getList("network");
+		if (networks == null)
+			throw new RuntimeException("No network directives found.");
 
-        Iterator<String> networkIt = networks.iterator();
-        while (networkIt.hasNext()) {
-            String network = networkIt.next();
-            ConfigFile netConfig = configFile.getSubConfig(network, true);
-            List<String> subnets = netConfig.getList("subnet");
-            if (subnets == null) {
-                LOGGER.info("No subnet directives for network " + network + " found. Skipping.");
-                networkIt.remove();
-                continue;
-            }
+		Iterator<String> networkIt = networks.iterator();
+		while (networkIt.hasNext()) {
+			String network = networkIt.next();
+			ConfigFile netConfig = configFile.getSubConfig(network, true);
+			List<String> subnets = netConfig.getList("subnet");
+			if (subnets == null) {
+				LOGGER.info("No subnet directives for network " + network + " found. Skipping.");
+				networkIt.remove();
+				continue;
+			}
 
-            Iterator<String> subnetIt = subnets.iterator();
-            while (subnetIt.hasNext()) {
-                String subnet = subnetIt.next();
-                ConfigFile subnetConfig = netConfig.getSubConfig(subnet, true);
-                if (subnetConfig.getList("channel") == null) {
-                    LOGGER.warn("No channel directives for subnet " + subnet + " found. Skipping.");
-                    subnetIt.remove();
-                    continue;
-                } else {
-                    page.addSubnet(network, subnet);
-                }
+			Iterator<String> subnetIt = subnets.iterator();
+			while (subnetIt.hasNext()) {
+				String subnet = subnetIt.next();
+				ConfigFile subnetConfig = netConfig.getSubConfig(subnet, true);
+				if (subnetConfig.getList("channel") == null) {
+					LOGGER.warn("No channel directives for subnet " + subnet + " found. Skipping.");
+					subnetIt.remove();
+					continue;
+				} else {
+					webApp.addSubnet(network, subnet);
+				}
 
-                String dataSource = subnetConfig.getString("dataSource");
-                AbstractPlotScheduler scheduler = plotScheduler.get(dataSource);
-                LOGGER.info("Assigning subnet " + subnet + " to " + dataSource);
-                scheduler.add(new SubnetPlotter(network, subnet, subnetConfig));
-            }
-            netConfig.putList("subnet", subnets);
-        }
-        configFile.putList("network", networks);
+				String dataSource = subnetConfig.getString("dataSource");
+				AbstractPlotScheduler scheduler = plotScheduler.get(dataSource);
+				LOGGER.info("Assigning subnet " + subnet + " to " + dataSource);
+				scheduler.add(new SubnetPlotter(network, subnet, subnetConfig));
+			}
+			netConfig.putList("subnet", subnets);
+		}
+		configFile.putList("network", networks);
 
-        Iterator<String> schedulerIt = plotScheduler.keySet().iterator();
-        while (schedulerIt.hasNext()) {
-            String server = schedulerIt.next();
-            AbstractPlotScheduler ps = plotScheduler.get(server);
-            if (ps.subnetCount() < 1) {
-                LOGGER.warn("No subnets feeding from " + ps.name + ". I'll prune it.");
-                schedulerIt.remove();
-            }
-        }
-    }
+		Iterator<String> schedulerIt = plotScheduler.keySet().iterator();
+		while (schedulerIt.hasNext()) {
+			String server = schedulerIt.next();
+			AbstractPlotScheduler ps = plotScheduler.get(server);
+			if (ps.subnetCount() < 1) {
+				LOGGER.warn("No subnets feeding from " + ps.name + ". I'll prune it.");
+				schedulerIt.remove();
+			}
+		}
+	}
 
-    /**
-     * Prune PlotSchedulers that have no subnets assigned to them.
-     */
-    private void pruneSchedulers() {
-        Iterator<String> schedulerIt = plotScheduler.keySet().iterator();
-        while (schedulerIt.hasNext()) {
-            String server = schedulerIt.next();
-            AbstractPlotScheduler ps = plotScheduler.get(server);
-            if (ps.subnetCount() < 1) {
-                LOGGER.warn("No subnets feeding from " + ps.name + ". I'll prune it.");
-                schedulerIt.remove();
-            }
-        }
-    }
+	/**
+	 * Prune PlotSchedulers that have no subnets assigned to them.
+	 */
+	private void pruneSchedulers() {
+		Iterator<String> schedulerIt = plotScheduler.keySet().iterator();
+		while (schedulerIt.hasNext()) {
+			String server = schedulerIt.next();
+			AbstractPlotScheduler ps = plotScheduler.get(server);
+			if (ps.subnetCount() < 1) {
+				LOGGER.warn("No subnets feeding from " + ps.name + ". I'll prune it.");
+				schedulerIt.remove();
+			}
+		}
+	}
 
-    /**
-     * Schedule a recurring call to produce plot jobs.
-     */
-    private void schedulePlots() {
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        for (AbstractPlotScheduler ps : plotScheduler.values()) {
+	/**
+	 * Schedule a recurring call to produce plot jobs.
+	 */
+	private void schedulePlots() {
+		ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+		for (AbstractPlotScheduler ps : plotScheduler.values()) {
 
-            // schedule first plot immediately
-            new Thread(ps).start();
+			// schedule first plot immediately
+			new Thread(ps).start();
 
-            if (ps instanceof RealtimePlotScheduler) {
-                // satrt automated plots at the top of the next period
-                int delay = SubnetPlotter.DURATION_S;
-                delay -= (System.currentTimeMillis() / 1000) % SubnetPlotter.DURATION_S;
-                LOGGER.debug("Scheduled plots start in " + delay + "ms");
-                scheduler.scheduleAtFixedRate(ps, delay, SubnetPlotter.DURATION_S, TimeUnit.SECONDS);
-            }
-        }
-    }
+			if (ps instanceof RealtimePlotScheduler) {
+				// satrt automated plots at the top of the next period
+				int delay = SubnetPlotter.DURATION_S;
+				delay -= (System.currentTimeMillis() / 1000) % SubnetPlotter.DURATION_S;
+				LOGGER.debug("Scheduled plots start in " + delay + "ms");
+				scheduler.scheduleAtFixedRate(ps, delay, SubnetPlotter.DURATION_S, TimeUnit.SECONDS);
+			}
+		}
+	}
 
-    public static void createConfig() throws IOException {
+	public static void createConfig() throws IOException {
 
-        InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("pensive-example.config");
-        FileOutputStream os = new FileOutputStream(DEFAULT_CONFIG_FILENAME);
-        try {
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = is.read(buffer)) > 0) {
-                os.write(buffer, 0, length);
-            }
-        } finally {
-            is.close();
-            os.close();
-        }
-    }
+		InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("pensive-example.config");
+		FileOutputStream os = new FileOutputStream(DEFAULT_CONFIG_FILENAME);
+		try {
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = is.read(buffer)) > 0) {
+				os.write(buffer, 0, length);
+			}
+		} finally {
+			is.close();
+			os.close();
+		}
+	}
 
-    public void stop() {
-    	for (AbstractPlotScheduler ps : plotScheduler.values()) 
-    		ps.stop();
-    }
-    
-    /**
-     * Where it all begins
-     * 
-     * @param args
-     */
-    public static void main(String[] args) {
-        Args config = null;
-        try {
-            config = new Args(args);
-        } catch (JSAPException e1) {
-            System.err.println("Couldn't parse command line. Try using the --help flag.");
-            System.exit(1);
-        }
+	public void stop() {
+		for (AbstractPlotScheduler ps : plotScheduler.values())
+			ps.stop();
+	}
 
-        if (config.createConfig) {
-            try {
-                LOGGER.warn("Creating example config " + DEFAULT_CONFIG_FILENAME);
-                Pensive.createConfig();
-            } catch (IOException e) {
-                LOGGER.warn("Cannot write example config. " + e.getLocalizedMessage());
-            }
-            System.exit(0);
-        }
+	/**
+	 * Where it all begins
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Args config = null;
+		try {
+			config = new Args(args);
+		} catch (JSAPException e1) {
+			System.err.println("Couldn't parse command line. Try using the --help flag.");
+			System.exit(1);
+		}
 
-        ConfigFile cf = new ConfigFile(config.configFileName);
-        if (!cf.wasSuccessfullyRead()) {
-            LOGGER.warn("Can't parse config file " + config.configFileName + ". Try using the --help flag.");
-            System.exit(1);
-        }
+		if (config.createConfig) {
+			try {
+				LOGGER.warn("Creating example config " + DEFAULT_CONFIG_FILENAME);
+				Pensive.createConfig();
+			} catch (IOException e) {
+				LOGGER.warn("Cannot write example config. " + e.getLocalizedMessage());
+			}
+			System.exit(0);
+		}
 
-        Pensive pensive = new Pensive(cf);
+		ConfigFile cf = new ConfigFile(config.configFileName);
+		if (!cf.wasSuccessfullyRead()) {
+			LOGGER.warn("Can't parse config file " + config.configFileName + ". Try using the --help flag.");
+			System.exit(1);
+		}
 
-        if (config.startTime == null) 
-            pensive.createPlotSchedulers();
-        else
-            pensive.createPlotSchedulers(config.startTime, config.endTime);
+		Pensive pensive = new Pensive(cf);
 
-        pensive.schedulePlots();
-        
-        if (config.startTime != null)
-        	pensive.stop();
+		if (config.startTime == null)
+			pensive.createPlotSchedulers();
+		else {
+			pensive.createPlotSchedulers(config.startTime, config.endTime);
+			try {
+				Thread.currentThread().sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			pensive.stop();
+		}
 
-    }
+	}
 }
